18d61826f4029719367ed6b9c04f221d33ccffb0
Luke Drogan <luke@super-wide.com>
2021-04-24 15:13:35 +0100

    Change structure and all questions coming through

--- index.js
+++ index.js
@@ -16,81 +16,69 @@ THEN I am taken to the corresponding section of the README
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// format taken from inquirer.js documentation:
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'Please enter your project title:',
-      name: 'title',
-    },
-    {
-      type: 'input',
-      message: 'Please enter a brief project description:',
-      name: 'description',
-    },
-    {
-      type: 'input',
-      message: 'Please enter installation for your project:',
-      name: 'installation',
-    },
-    {
-      type: 'input',
-      message: 'Please enter usage information for your project:',
-      name: 'usage',
-    },
-    {
-      type: 'input',
-      message: 'Please enter any contribution guidelines for your project:',
-      name: 'contribution',
-    },
-    {
-      type: 'input',
-      message: 'Please enter test instructions for your project:',
-      name: 'test',
-    },
-    {
-      type: 'list',
-      message: 'Please choose the license for your project:',
-      name: 'license',
-      choices: [
-        "No License",
-        "License 1",
-        "License 2",
-        "License 3",
-        "License 4",
-      ]},
-      {
-        type: 'input',
-        message: 'Please enter your GitHub username:',
-        name: 'username',
-      },
-      {
-        type: 'input',
-        message: 'Please enter your email address:',
-        name: 'email',
-      },
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
-
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'Please enter your project title:',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Please enter a brief project description:',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Please enter installation for your project:',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Please enter usage information for your project:',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Please enter any contribution guidelines for your project:',
+    name: 'contribution',
+  },
+  {
+    type: 'input',
+    message: 'Please enter test instructions for your project:',
+    name: 'test',
+  },
+  {
+    type: 'list',
+    message: 'Please choose the license for your project:',
+    name: 'license',
+    choices: [
+      "No License",
+      "License 1",
+      "License 2",
+      "License 3",
+      "License 4",
+    ]},
+    {
+      type: 'input',
+      message: 'Please enter your GitHub username:',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your email address:',
+      name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(responses => console.log(responses))
+
+}
 
 // Function call to initialize app
 init();
